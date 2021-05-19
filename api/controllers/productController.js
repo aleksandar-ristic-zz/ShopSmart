193d15d60e615a448abb95144eb0931fb958650b
Aleksandar Ristic <aristic665@gmail.com>
2021-05-19 03:00:16 +0200

    Finished product routes

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product')
 
-// Create new product => /api/v1/product/new
+// Create new product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
 
   const product = await Product.create(req.body);
@@ -11,9 +11,75 @@ exports.newProduct = async (req, res, next) => {
   });
 }
 
-exports.getProducts = (req, res, next) => {
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+  const products = await Product.find();
+
   res.status(200).json({
     success: true,
-    message: 'This route will show all products in database'
+    count: products.length,
+    products
+  });
+}
+
+// Get single product details => /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+}
+
+// Update product => /api/v1/admin/product/:id
+exports.updateProduct = async (req, res) => {
+
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    useFindAndModify: false
+  });
+
+  res.status(200).json({
+    success: true,
+    product
+  });
+}
+
+// Delete product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res) => {
+  
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    sucess: true,
+    message: 'Product is deleted'
   });
 }
